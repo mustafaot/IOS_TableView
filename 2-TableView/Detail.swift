01732f0781e3f40498223a4737a6bf4625b5291d
Mustafa OT <mustafa.ot@GLTMAC105.local>
2019-09-06 00:38:55 -0700

    first commit for tableview

---
+++ 2-TableView/Detail.swift
@@ -0,0 +1,30 @@
+//
+//  Detail.swift
+//  2-TableView
+//
+//  Created by Mustafa OT on 2.09.2019.
+//  Copyright © 2019 Mustafa OT. All rights reserved.
+//
+
+import UIKit
+
+class Detail: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
